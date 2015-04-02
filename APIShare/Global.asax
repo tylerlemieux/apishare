c13aecc3718d2419ab967513a152ad87c1c12117
tylerlemieux <tlemi764@mail.endicott.edu>
2015-04-02 11:26:39 -0400

    initial checkin of apishare

---
+++ APIShare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIShare.MvcApplication" Language="C#" %>
---
+++ APIShare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APIShare.Startup))]
+namespace APIShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
