eb3b06b6e699c42e261c61e6481e823084253fd4
tylerlemieux <tlemi764@mail.endicott.edu>
2015-04-06 14:04:07 -0400

    added authentication controller

---
+++ APIShare/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIShare.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        /// <summary>
+        /// Takes in a username and password
+        /// </summary>
+        /// <returns>Json object with whether or not the login succeeded</returns>
+        [HttpPost]
+        [ActionName("Login")]
+        public JsonResult LoginSubmit(string username, string password)
+        {
+            return Json("");
+        }
+
+        [HttpPost]
+        [ActionName("Registration")]
+        public JsonResult RegistrationSubmit()
+        {
+            return Json("");
+        }
+
+
+    }
+}
