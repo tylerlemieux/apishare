8110dea74ae16d7a19cb4f86ea406616436e88a5
tylerlemieux <tlemi764@mail.endicott.edu>
2015-04-08 13:19:11 -0400

    added login, register, and logout funtions

--- APIShare/Controllers/AuthenticationController.cs
+++ APIShare/Controllers/AuthenticationController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using APIShare.Models;
+using APIShare.Models.Helpers;
+using System.Security.Cryptography;
 
 namespace APIShare.Controllers
 {
     public class AuthenticationController : Controller
     {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
         /// <summary>
         /// Takes in a username and password
         /// </summary>
@@ -16,16 +29,86 @@ namespace APIShare.Controllers
         [ActionName("Login")]
         public JsonResult LoginSubmit(string username, string password)
         {
+            using(APIToolEntities context = new APIToolEntities())
+            {
+                User user = context.Users.Where(s => s.Username == username).FirstOrDefault();
+                if(user != null)
+                {
+                    string hashedPass = SecurityHelper.GetHash(password, user.Salt);
+
+                    user = context.Users.Where(u => u.Username == username && u.Password == hashedPass).FirstOrDefault();
+                    if(user != null)
+                    {
+                        //User exists... start the session
+                        Session["UserID"] = user.UserID;
+                    }
+                    else
+                    {
+                        //password is incorrect
+                        return Json(new { Success = false, ErrorMessage = "Login failed" });
+                    }
+                }
+                else
+                {
+                    //username doesnt exist
+                    return Json(new { Success = false, ErrorMessage = "Login failed" });
+                }
+                
+            }
+
             return Json("");
         }
 
         [HttpPost]
         [ActionName("Registration")]
-        public JsonResult RegistrationSubmit()
+        public JsonResult RegistrationSubmit(string username, string password, string email)
         {
-            return Json("");
+            using(APIToolEntities context = new APIToolEntities())
+            {
+                if (username != null && password != null && email != null)
+                {
+                    var usersWithEmailAndPass = context.Users.Where(s => username == s.Username || email == s.Email);
+                    if (usersWithEmailAndPass.Count() == 0)
+                    {
+                        //User doesnt already exist, so create the user
+                        Models.User user = new User();
+                        user.Username = username;
+
+                        string salt = SecurityHelper.GetSalt();
+                        user.Salt = salt;
+                        user.Password = SecurityHelper.GetHash(password, salt);
+                        user.Email = email;
+
+                        context.Users.Add(user);
+                        context.SaveChanges();
+
+                        return Json(new { Success = true });
+                    }
+                    else
+                    {
+                        return Json(new { Success = false, ErrorMessage = "Username or password already exists" });
+                    }
+                }
+                else
+                {
+                    return Json(new { Success = false, ErrorMessage = "Must supply username, password, and email" });
+                }
+            }
         }
 
+        [HttpDelete]
+        public JsonResult Logout()
+        {
+            try
+            {
+                Session.Clear();
+                return Json(new {Success = true});
+            }
+            catch(Exception ex)
+            {
+                return Json(new { Success = false, ErrorMessage = ex });
+            }
+        }
 
     }
 }
---
+++ APIShare/Models/Helpers/SecurityHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+using System.Security;
+
+namespace APIShare.Models.Helpers
+{
+    public static class SecurityHelper
+    {
+        public static string GetSalt()
+        {
+            //Create and populate random byte array
+            byte[] randomArray = new byte[20];
+            string randomString;
+
+            //Create random salt and convert to string
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(randomArray);
+            randomString = Convert.ToBase64String(randomArray);
+            return randomString;
+        }
+
+        public static string GetHash(string password, string salt)
+        {
+            using (SHA512CryptoServiceProvider sha512 = new SHA512CryptoServiceProvider())
+            {
+                byte[] data = System.Text.Encoding.ASCII.GetBytes(password + salt);
+                data = sha512.ComputeHash(data);
+                string SHA512Hash = System.Text.Encoding.ASCII.GetString(data);
+
+                return SHA512Hash;
+            }
+        }
+    }
+}
