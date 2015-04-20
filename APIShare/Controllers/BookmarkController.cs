73532c38f70bf5df539257322013f3439074450a
tylerlemieux <tlemi764@mail.endicott.edu>
2015-04-20 15:01:27 -0400

    Functions for adding bookmarks to a user's profile and functions for searching users and libraries

--- APIShare/Controllers/BookmarkController.cs
+++ APIShare/Controllers/BookmarkController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using APIShare.Models;
 
 namespace APIShare.Controllers
 {
@@ -25,7 +26,7 @@ namespace APIShare.Controllers
         [HttpPost]
         public JsonResult CreateBookmark()
         {
-            throw new NotImplementedException();
+
             return Json(false);
         }
 
@@ -34,10 +35,34 @@ namespace APIShare.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpDelete]
-        public JsonResult RemoveBookmark()
+        public JsonResult RemoveBookmark(int bookmarkId)
         {
-            throw new NotImplementedException();
-            return Json(false);
+            if (Session["UserID"] != null)
+            {
+                using (APIToolEntities context = new APIToolEntities())
+                {
+                    var bookmarkToDelete =
+                    (from ub in context.UserBookmarks
+                     where bookmarkId == ub.BookmarkID && (int) Session["UserID"] == ub.UserID
+                     select ub).FirstOrDefault();
+
+                    if(bookmarkToDelete != null)
+                    {
+                        context.UserBookmarks.Remove(bookmarkToDelete);
+                        context.SaveChanges();
+
+                        return Json(new { Success = true });
+                    }
+                    else
+                    {
+                        return Json(new { Success = false, ErrorMessage = "Couldn't find user's bookmark" });
+                    }
+                }
+            }
+            else
+            {
+                return Json(new { Success = false, ErrorMessage = "No user logged in" });
+            }
         }
 
         /// <summary>
@@ -45,10 +70,29 @@ namespace APIShare.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpPost]
-        public JsonResult AddBookmark()
+        public JsonResult AddBookmark(int bookmarkId)
         {
-            throw new NotImplementedException();
-            return Json(false);
+            if (Session["UserID"] != null)
+            {
+                int userId = (int)Session["UserID"];
+                using (APIToolEntities context = new APIToolEntities())
+                {
+                    UserBookmark bookmarkToAdd = new UserBookmark();
+                    bookmarkToAdd.BookmarkID = bookmarkId;
+                    bookmarkToAdd.DateAdded = DateTime.Now;
+                    bookmarkToAdd.Favorited = "N";
+                    bookmarkToAdd.UserID = userId;
+
+                    context.UserBookmarks.Add(bookmarkToAdd);
+                    context.SaveChanges();
+
+                    return Json(new { Success = true });
+                }
+            }
+            else
+            {
+                return Json(new { Success = false, ErrorMessage = "No user logged in" });
+            }
         }
     }
 }
--- APIShare/Models/Search/SearchModel.cs
+++ APIShare/Models/Search/SearchModel.cs
@@ -14,8 +14,51 @@ namespace APIShare.Models.Search
         /// <returns>SearchVM with list of users and libraries</returns>
         public static SearchVM Search(string searchString) 
         {
+            SearchVM viewModel = new SearchVM();
+            var libraries = SearchLibraries(searchString);
+            var users = SearchUsers(searchString);
 
-            return new SearchVM();
+
+            return viewModel;
+        }
+
+        public static List<SearchLibraryResult> SearchLibraries(string searchString)
+        {
+            using (APIToolEntities context = new APIToolEntities())
+            {
+                var searchResult = 
+                (from l in context.Bookmarks
+                 where l.Name.Contains("/" + searchString + "/")
+                    || l.Description.Contains("/" + searchString + "/")
+                 select new SearchLibraryResult
+                 {
+                     LibraryID = l.BookmarkID,
+                     LibraryName = l.Name,
+                     Description = l.Description
+                 }).ToList();
+
+                return searchResult;
+            }
+        }
+        public static List<SearchUserResult> SearchUsers(string searchString)
+        {
+            using (APIToolEntities context = new APIToolEntities())
+            {
+                var searchResult =
+                (from u in context.Users
+                 where u.Username.Contains("/" + searchString + "/")
+                    || u.Name.Contains("/" + searchString + "/")
+                 select new SearchUserResult
+                 {
+                     UserID = u.UserID,
+                     Username = u.Username,
+                     Avatar = u.Avatar,
+                     Bio = u.Bio, 
+                     Name = u.Name
+                 }).ToList();
+
+                return searchResult;
+            }
         }
     }
 
